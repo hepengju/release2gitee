0232a6cd587c37dc649c4043a1faa4d00554a204
hepengju <he_pengju@163.com>
2025-12-20 19:57:50 +0800

    初步测试

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use anyhow::bail;
 use clap::Parser;
 use env_logger::Env;
-use log::info;
-use reqwest::blocking::{Client, Response};
+use log::{error, info};
+use reqwest::blocking::{Client, Response, multipart};
 use serde::{Deserialize, Serialize};
+use std::fs;
+use std::fs::write;
+use std::path::Path;
 
 const GITHUB_API_URL: &str = "https://api.github.com/repos";
 const GITEE_API_URL: &str = "https://gitee.com/api/v5/repos";
@@ -23,6 +26,16 @@ fn main() -> anyhow::Result<()> {
     let gitee_releases = gitee_releases(client, cli)?;
 
     // 3.只同步缺失的releases信息 (以tag_name为唯一标识)
+    let releases = new_releases(github_releases, gitee_releases);
+
+    // 4.逐个创建gitee的release信息并上传附件
+    for release in releases {
+        gitee_release_create(client, cli, &release)?;
+    }
+    Ok(())
+}
+
+fn new_releases(github_releases: Vec<Release>, gitee_releases: Vec<Release>) -> Vec<Release> {
     let releases = github_releases
         .into_iter()
         .filter(|github_release| {
@@ -32,11 +45,8 @@ fn main() -> anyhow::Result<()> {
                 .is_none()
         })
         .collect::<Vec<_>>();
-    info!("Gitee中缺失的release个数: {}", releases.len());
-
-
-
-    Ok(())
+    info!("Gitee中缺失的releases个数: {}", releases.len());
+    releases
 }
 
 fn github_releases(client: &Client, cli: &Cli) -> anyhow::Result<Vec<Release>> {
@@ -49,12 +59,16 @@ fn github_releases(client: &Client, cli: &Cli) -> anyhow::Result<Vec<Release>> {
         .send()?;
 
     if !response.status().is_success() {
-        bail!("Github仓库Releases信息获取失败!")
+        bail!("Github仓库releases获取失败!")
     }
 
     let result = response.text()?;
     let releases: Vec<Release> = serde_json::from_str(&result)?;
-    info!("Github仓库Releases信息获取最近的{}个成功", releases.len());
+    info!(
+        "Github仓库releases获取最近的{}个成功: {}",
+        releases.len(),
+        get_tag_names(&releases)
+    );
     Ok(releases)
 }
 
@@ -67,18 +81,33 @@ fn gitee_releases(client: &Client, cli: &Cli) -> anyhow::Result<Vec<Release>> {
         .send()?;
 
     if !response.status().is_success() {
-        bail!("Gitee仓库Releases信息获取失败!")
+        bail!("Gitee仓库releases信息获取失败!")
     }
 
     let result = response.text()?;
     let releases: Vec<Release> = serde_json::from_str(&result)?;
-    info!("Gitee仓库Releases信息获取最近的{}个成功", releases.len());
+    info!(
+        "Gitee仓库releases信息获取{}个: {}",
+        releases.len(),
+        get_tag_names(&releases)
+    );
     Ok(releases)
+}
+
+fn get_tag_names(releases: &Vec<Release>) -> String {
+    releases
+        .iter()
+        .map(|release| release.tag_name.as_str())
+        .collect::<Vec<_>>()
+        .join(", ")
 }
 
 fn gitee_release_create(client: &Client, cli: &Cli, release: &Release) -> anyhow::Result<()> {
     let response: Response = client
-        .post(format!("{}/{}/{}/releases", GITEE_API_URL, cli.gitee_owner, cli.gitee_repo))
+        .post(format!(
+            "{}/{}/{}/releases",
+            GITEE_API_URL, cli.gitee_owner, cli.gitee_repo
+        ))
         .header("Authorization", format!("token {}", cli.gitee_token))
         .header("Content-Type", "application/json")
         .json(release)
@@ -88,13 +117,62 @@ fn gitee_release_create(client: &Client, cli: &Cli, release: &Release) -> anyhow
         bail!("Gitee仓库Release创建失败: {}!", &release.tag_name)
     }
 
-    info!("Gitee仓库Release创建成功: {}, 开始上传附件!", &release.tag_name);
+    info!(
+        "Gitee仓库Release创建成功: {}, 开始处理附件!",
+        &release.tag_name
+    );
 
-    // 下载github附件
+    // 下载github附件到本地
+    for asset in &release.assets {
+        let response = client
+            .get(&asset.browser_download_url)
+            .header("User-Agent", USER_AGENT)
+            .send()?;
 
+        if response.status().is_success() {
+            let file_content = response.bytes()?;
+            write(&asset.name, file_content)?;
+            info!("下载附件 {} 成功", &asset.name);
+        } else {
+            error!("下载附件 {} 失败", &asset.name);
+        }
+    }
 
     // 上传到gitee
+    for asset in &release.assets {
+        // 检查文件是否存在
+        if !Path::new(&asset.name).exists() {
+            error!("本地文件 {} 不存在，跳过上传", &asset.name);
+            continue;
+        }
 
+        // 构造上传URL
+        let upload_url = format!(
+            "{}/{}/{}/releases/{}/attach_files",
+            GITEE_API_URL, cli.gitee_owner, cli.gitee_repo, release.tag_name,
+        );
+
+        let form = multipart::Form::new().file("file", &asset.name)?;
+
+        // 上传文件到Gitee
+        let upload_response = client
+            .post(&upload_url)
+            .header("Authorization", format!("token {}", cli.gitee_token))
+            .header("Content-Type", "application/octet-stream")
+            .multipart(form)
+            .send()?;
+
+        if upload_response.status().is_success() {
+            info!("上传附件 {} 到Gitee成功", &asset.name);
+
+            // 删除本地临时文件
+            if let Err(e) = fs::remove_file(&asset.name) {
+                error!("删除临时文件 {} 失败: {}", &asset.name, e);
+            }
+        } else {
+            error!("上传附件 {} 到Gitee失败: ", &asset.name,);
+        }
+    }
 
     Ok(())
 }
@@ -116,14 +194,13 @@ pub struct Cli {
     #[clap(env = "gitee_token")]
     gitee_token: String,
 
-    #[clap(default_value_t = 5)]
+    #[clap(default_value_t = 1)]
     lastest_release_count: u8,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Assert {
     name: String,
-    size: Option<u64>,
     browser_download_url: String,
 }
 

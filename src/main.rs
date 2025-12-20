47cf3cbe65fcf2679730eb14457535a91105a5cf
hepengju <he_pengju@163.com>
2025-12-20 18:18:56 +0800

    加入依赖，解析配置

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+use env_logger::Env;
+use log::{info};
+
 fn main() {
-    println!("Hello, world!");
+    // 默认日志级别改为INFO
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    let cli = Cli::parse();
+    info!("配置: {cli:?}");
+}
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    #[clap(env="github_owner")]
+    github_owner: String,
+
+    #[clap(env="github_repo")]
+    github_repo: String,
+
+    #[clap(env="gitee_owner")]
+    gitee_owner: String,
+
+    #[clap(env="gitee_repo")]
+    gitee_repo: String,
+
+    #[clap(env="gitee_token")]
+    gitee_token: String,
 }

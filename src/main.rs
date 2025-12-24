fe9547eae1c065e764bce1a97d4a030b0f155b4a
hepengju <he_pengju@163.com>
2025-12-24 13:51:35 +0800

    命令行解析完成的日志显示

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,14 @@ use release2gitee::{check_cli, sync_github_releases_to_gitee};
 
 // [Rust 中的命令行应用程序](https://cli.rust-lang.net.cn/book/index.html)
 fn main() -> anyhow::Result<()> {
-    // 参数解析
+    // 参数解析和日志配置
     let cli = &Cli::parse();
-    info!("命令行解析完成: {cli}");
-    check_cli(cli)?;
-
-    // 日志配置
     env_logger::Builder::new()
         .filter_level(cli.verbosity.into())
         .init();
+
+    info!("命令行解析完成: {cli}");
+    check_cli(cli)?;
 
     // 同步程序
     sync_github_releases_to_gitee(cli)?;
